6f91407b5d19c7bb068cd5d1262d89fa14ab56b8
pcsalvo <pcsalvo@uc.cl>
2021-05-24 15:45:32 -0400

    mapa listo

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,8 +1,24 @@
-import React from 'react'
-import { MapContainer, TileLayer } from "react-leaflet";
+import React, { useEffect, useState } from 'react'
+import { MapContainer, TileLayer, Polyline, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
 const MapView = ({socket}) => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        socket.on('FLIGHTS', (vuelos) => {setData(vuelos)});
+        socket.emit('FLIGHTS');
+    }, [])
+
     return(
     <div>
         <MapContainer
@@ -12,6 +28,22 @@ const MapView = ({socket}) => {
         <TileLayer 
         url='https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png' 
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' />
+        {
+            data.map((flight) => 
+            <div>
+                <Marker position={flight.origin}>
+                <Popup>
+                    Origen del vuelo {flight.code}
+                </Popup>
+                </Marker>
+                <Marker position={flight.destination}>
+                <Popup>
+                    Destino del vuelo {flight.code}
+                </Popup>
+                </Marker>
+                <Polyline pathOptions={{ color: getRandomColor() }} positions={[flight.origin, flight.destination]} />
+            </div>
+        )}
         </MapContainer>
     </div>)
 }
